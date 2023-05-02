d3f6649740b60164843734179349dbef77f04062
KonradRussa <konrad.russa@gmail.com>
2023-05-02 07:56:18 +0200

    potential operator fix

--- src/player.h
+++ src/player.h
@@ -5,6 +5,7 @@
 
 #include <SDL2/SDL.h>
 #include <memory>
+#include <vector>
 
 class Sprite {
 public:
@@ -110,6 +111,7 @@ Player &Player::operator=(Player &other) {
     point = other.point;
     if (obstacles) {
       delete obstacles;
+      *obstacles = std::vector<std::shared_ptr<Sprite>>();
     }
     *obstacles = *other.obstacles;
     _worldSize = other._worldSize;
@@ -122,6 +124,7 @@ Player &Player::operator=(Player &&other) {
     point = std::move(other.point);
     if (obstacles) {
       delete obstacles;
+      *obstacles = std::vector<std::shared_ptr<Sprite>>();
     }
     _worldSize = other._worldSize;
     other._worldSize = 0;
